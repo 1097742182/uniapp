221f7cd82a5ce41b9863b5cf3e014b94a0a2cef3
administrator <1097742182@qq.com>
2023-05-31 15:52:11 +0800

    基本完成userCountCard的样式

--- pagesGames/components/gameMenu/UserCountCard.vue
+++ pagesGames/components/gameMenu/UserCountCard.vue
@@ -14,6 +14,11 @@
       <view class="bottomLeft">我的排名：{{ UserRank }}</view>
       <view class="bottomRight">积分明细 ></view>
     </view>
+
+    <view class="screen_background">
+      <view class="screen_background_shape1"></view>
+      <view class="screen_background_shape2"></view>
+    </view>
   </view>
 </template>
 
@@ -44,8 +49,9 @@ export default {
   padding-bottom: 40px;
   box-sizing: border-box;
 
-  background: #406de4;
-  background-image: linear-gradient(90deg, #8e6 10%, rgba(0, 0, 0, 0) 10%);
+  // background: #406de4;
+  // background-image: linear-gradient(90deg, #8e6 10%, rgba(0, 0, 0, 0) 10%);
+  background: linear-gradient(90deg, #406de4, #7c78b8);
   background-repeat: repeat-y;
 
   display: flex;
@@ -53,6 +59,8 @@ export default {
 
   .left {
     flex: 1;
+    z-index: 10;
+
     .leftTitle {
       color: #fff;
       font-size: 18px;
@@ -70,6 +78,7 @@ export default {
   .right {
     margin: auto;
     margin-right: 10px;
+    z-index: 10;
 
     .iconContent {
       padding: 8px;
@@ -80,6 +89,7 @@ export default {
 
   .bottom {
     position: absolute;
+    z-index: 10;
     bottom: 0;
     height: 40px;
     width: 100%;
@@ -96,6 +106,41 @@ export default {
     .bottomRight {
       margin: auto 0;
     }
+  }
+}
+
+.screen_background {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 0;
+
+  .screen_background_shape1 {
+    height: 200px;
+    width: 200px;
+    background: #7e7bb9;
+    top: -105px;
+    right: -44px;
+    border-radius: 60px;
+    z-index: 0;
+
+    transform: rotate(45deg);
+    position: absolute;
+  }
+
+  .screen_background_shape2 {
+    height: 100px;
+    width: 100px;
+    background: #5ca6f34d;
+    top: -50px;
+    left: -14px;
+    border-radius: 50%;
+    z-index: 0;
+
+    transform: rotate(45deg);
+    position: absolute;
   }
 }
 </style>
