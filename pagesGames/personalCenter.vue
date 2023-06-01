17bc5efa21909abc38eee331e6c5786df26dd352
administrator <1097742182@qq.com>
2023-06-01 14:49:06 +0800

    基本完成个人中心界面

---
+++ pagesGames/personalCenter.vue
@@ -0,0 +1,152 @@
+<template>
+  <view class="personalCenter">
+    <cl-header title="个人中心" :transparent="true" />
+
+    <u-cell-group>
+      <u-cell icon="account" title="头像" isLink>
+        <view class="imageContent" slot="value" style="position: relative">
+          <button
+            type="balanced"
+            open-type="chooseAvatar"
+            @chooseavatar="onChooseavatar"
+            class="buttonClass"
+          >
+            <image :src="avatarUrl" class="refreshIcon"> </image>
+          </button>
+        </view>
+      </u-cell>
+
+      <u-cell icon="order" title="用户名" isLink>
+        <input
+          type="nickname"
+          slot="value"
+          style="text-align: right"
+          v-model="nickName"
+        />
+      </u-cell>
+
+      <u-cell
+        @click="chooseGender()"
+        icon="man"
+        title="性别"
+        :value="genderValue"
+        isLink
+      >
+      </u-cell>
+
+      <u-cell
+        @click="chooseCity()"
+        icon="map"
+        title="城市"
+        :value="cityValue"
+        isLink
+      >
+      </u-cell>
+    </u-cell-group>
+
+    <u-picker
+      ref="uPicker"
+      :show="pickerShow"
+      :columns="columns"
+      :defaultIndex="[0]"
+      @cancel="cancel"
+      @confirm="confirm"
+      @change="change"
+    ></u-picker>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      avatarUrl: uni.getStorageSync("avatarUrl"),
+      nickName: uni.getStorageSync("nickName") || "",
+      pickerType: "",
+      pickerShow: false,
+      columns: [],
+      genderValue: "男",
+      genderColumns: [["男", "女"]],
+      cityValue: "",
+      cityColumns: [["广东"]],
+      cityChildColumns: [],
+    };
+  },
+  mounted() {
+    if (!this.avatarUrl) this.avatarUrl = this.ErrorAvatarUrl;
+    this._initCityColumns();
+  },
+  methods: {
+    _initCityColumns() {
+      const allCity = this.$api.user.getCity();
+      const firstColumns = Object.keys(allCity);
+      this.cityChildColumns = [];
+      for (const city of firstColumns) {
+        this.cityChildColumns.push(allCity[city]);
+      }
+      this.cityColumns = [firstColumns, this.cityChildColumns[0]];
+    },
+    chooseGender() {
+      this.pickerType = "gender";
+      this.columns = this.genderColumns;
+      this.pickerShow = true;
+    },
+    chooseCity() {
+      this.pickerType = "city";
+      this.columns = this.cityColumns;
+      this.pickerShow = true;
+    },
+    changeHandler1(e) {
+      const {
+        columnIndex,
+        value,
+        values,
+        index,
+        // 微信小程序无法将picker实例传出来，只能通过ref操作
+        picker = this.$refs.uPicker,
+      } = e;
+      if (columnIndex === 0) {
+        picker.setColumnValues(1, this.cityChildColumns[index]);
+      }
+    },
+    change(e) {
+      if (this.pickerType === "city") this.changeHandler1(e);
+      console.log("change", e);
+    },
+    confirm(e) {
+      console.log(e);
+      this[`${this.pickerType}Value`] = e.value.join(" ");
+      this.pickerShow = false;
+    },
+    cancel() {
+      this.pickerShow = false;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.personalCenter {
+  background-image: linear-gradient(
+    to bottom,
+    rgb(165, 207, 244) 0%,
+    white 100%
+  );
+  background-size: 100% 200px;
+  background-repeat: no-repeat;
+}
+
+.refreshIcon {
+  width: 60rpx;
+  height: 60rpx;
+  border-radius: 50%;
+  position: relative;
+}
+
+.buttonClass {
+  background: #ffffff00;
+  border: none;
+  line-height: 1;
+  border-radius: 100%;
+}
+</style>
