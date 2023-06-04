7f1bda95ceea863a12140b21369626bfa1c4c99e
幻境丶如梦 <1097742182@qq.com>
2023-06-04 23:47:25 +0800

    设置开发环境与生产环境的接口

--- common/config.js
+++ common/config.js
@@ -1,11 +1,11 @@
 const CONFIG = {
 	// 开发环境配置
 	development: {
-		baseUrl: 'https://8553130f-d188-40fd-8274-56ae596e446d.bspapp.com',
+		baseUrl: 'http://127.0.0.1:8000/',
 	},
 	// 生产环境配置
 	production: {
-		baseUrl: 'https://8553130f-d188-40fd-8274-56ae596e446d.bspapp.com',
+		baseUrl: 'https://www.xinyongji.com:9898/',
 	}
 }
 export default CONFIG[process.env.NODE_ENV];
