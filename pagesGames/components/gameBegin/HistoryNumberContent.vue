76c52f173a1dc2465fc78ee3550e74a6bd940bdf
administrator <1097742182@qq.com>
2023-06-06 14:30:49 +0800

    改成空格缩进

--- pagesGames/components/gameBegin/HistoryNumberContent.vue
+++ pagesGames/components/gameBegin/HistoryNumberContent.vue
@@ -1,46 +1,50 @@
 <template>
-	<view class="HistoryNumberContent">
-		<history-number-content-item v-for="item in countList" :value="item" :key="item"
-			class="contentItem"></history-number-content-item>
-	</view>
+  <view class="HistoryNumberContent">
+    <history-number-content-item
+      v-for="item in countList"
+      :value="item"
+      :key="item"
+      class="contentItem"
+    ></history-number-content-item>
+  </view>
 </template>
 
 <script>
-	import HistoryNumberContentItem from "./HistoryNumberContentItem.vue"
+import HistoryNumberContentItem from "./HistoryNumberContentItem.vue";
 
-	export default {
-		data() {
-			return {
-				countList: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-			}
-		},
+export default {
+  data() {
+    return {
+      countList: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+    };
+  },
 
-		mounted() {
-			// 根据传入的count对countList进行分隔
-			const count = this.HistoryNumberCount
-			this.countList = this.countList.slice(0, count)
-		},
-		components: {
-			HistoryNumberContentItem
-		}
-	}
+  mounted() {
+    // 根据传入的count对countList进行分隔
+    const count = this.HistoryNumberCount;
+    this.countList = this.countList.slice(0, count);
+  },
+  components: {
+    HistoryNumberContentItem,
+  },
+};
 </script>
 
 <style>
-	.HistoryNumberContent {
-		/* height: 300px; */
-		border: 1px solid red;
-		margin: 10px;
-		padding: 10px;
-		border-radius: 5px;
-		padding-bottom: 0;
+.HistoryNumberContent {
+  /* height: 300px; */
+  border: 1px solid red;
+  margin: 10px;
+  padding: 10px;
+  border-radius: 5px;
+  padding-bottom: 0;
 
-		display: flex;
-		flex-wrap: wrap;
-		justify-content: space-between;
-	}
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
 
-	.contentItem {
-		width: 48%;
-	}
+.contentItem {
+  width: 48%;
+}
 </style>
