5d17adef6e73583b6273368e079a66bf27d4da1a
administrator <1097742182@qq.com>
2023-06-06 09:28:59 +0800

    解决微信小程序 无法进入countMall的问题

--- pagesGames/countMall.vue
+++ pagesGames/countMall.vue
@@ -12,7 +12,7 @@
 </template>
 
 <script>
-import MallTicket from "@/pagesCountMall/components/MallTicket.vue";
+import MallTicket from "./components/countMall/MallTicket.vue";
 import UserCountCard from "@/pagesGames/components/gameMenu/UserCountCard.vue";
 
 export default {
