8a69d9c66cedd95bf584309042e23abf450b1151
administrator <1097742182@qq.com>
2023-06-06 16:02:35 +0800

    格式化代码

--- common/config.js
+++ common/config.js
@@ -1,12 +1,12 @@
 const CONFIG = {
-	// 开发环境配置
-	development: {
-		baseUrl: 'http://127.0.0.1:8000/',
-		// baseUrl: 'https://www.xinyongji.com:9898/',
-	},
-	// 生产环境配置
-	production: {
-		baseUrl: 'https://www.xinyongji.com:9898/',
-	}
-}
+  // 开发环境配置
+  development: {
+    baseUrl: "http://127.0.0.1:8000/",
+    // baseUrl: "https://www.xinyongji.com:9898/",
+  },
+  // 生产环境配置
+  production: {
+    baseUrl: "https://www.xinyongji.com:9898/",
+  },
+};
 export default CONFIG[process.env.NODE_ENV];
